98af86b26762c7a76bcc3ec19824274a326271d4
Ariel Nestor Saldana <mikeomfgg@gmail.com>
2016-10-20 19:23:50 -0400

    clear-master

---
+++ synergy/clear-master/index.js
@@ -0,0 +1,11 @@
+var sass = require('node-sass');
+
+// sass.render({
+//   file: scss_filename,
+//   [, options..]
+// }, function(err, result) { /*...*/ });
+// // OR
+// var result = sass.renderSync({
+//   data: scss_content
+//   [, options..]
+// });
