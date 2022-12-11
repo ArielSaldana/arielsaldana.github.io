55f39ca909ae39c64cad084192d57d9cd546f983
Ariel Saldana <ariel@unreal.io>
2022-12-11 18:32:49 -0500

    NextJS Upgrade for personal site

---
+++ next-js/next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const path = require('path')
+
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles'), path.join(__dirname, 'components')],
+  },
+}
+
+module.exports = nextConfig
